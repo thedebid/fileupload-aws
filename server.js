33f15c8a0c5f11fedac90886f99fae7aecb86e1e
Debid Magar <linkwithdm@gmail.com>
2021-08-17 20:21:59 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const AWS = require("aws-sdk");
+
+// Enter copied or downloaded access id and secret here
+const ID = "process.env.AWS_ACCESS_KEY";
+const SECRET = "process.env.AWS_SECRET";
+
+// Enter the name of the bucket that you have created here
+const BUCKET_NAME = "test-bucket-1242tsr";
+
+// Initializing S3 Interface
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
+});
+
+const uploadFile = (fileName) => {
+  // read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // setting up s3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: "cat.jpg", // file name you want to save as
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+// Enter the file you want to upload here
+uploadFile("cat.jpg");
